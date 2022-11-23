154b5efccc70a6fa78a9db9919f3af6d84b6845c
SangHwan <skyi0909@naver.com>
2022-11-23 12:16:07 +0900

    ui추가

--- Memo/src/main/java/userinterface/maininterface.java
+++ Memo/src/main/java/userinterface/maininterface.java
@@ -1,2 +1,8 @@
-package userinterface;public class maininterface {
+package userinterface;
+public class maininterface {
+    public void run(){
+        System.out.println("-----------------입력 (1번) 메뉴 생성-----------------");
+        System.out.println("-----------------입력 (2번) 메뉴 목록 조회-------------");
+        System.out.println("-----------------입력 (q) 시스템 종료-----------------");
+    }
 }
