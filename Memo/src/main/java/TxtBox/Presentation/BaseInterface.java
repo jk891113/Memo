e9ec20f8f74d5c69aad9f3de3bf1c1ea9ce69dd5
Park Seowoo <5w31892p@gmail.com>
2022-11-23 12:30:52 +0900

    폴더 수정

--- Memo/src/main/java/TxtBox/Presentation/BaseInterface.java
+++ Memo/src/main/java/TxtBox/Presentation/BaseInterface.java
@@ -1,4 +1,11 @@
 package TxtBox.Presentation;
 
 public class BaseInterface {
+    public void run() {
+        System.out.println("-----------------입력 (1번)-----------------");
+        System.out.println("-----------------출력 (2번)-----------------");
+        System.out.println("-----------------수정 (3번)-----------------");
+        System.out.println("-----------------삭제 (4번)-----------------");
+        System.out.println("-----------------종료 (5번)-----------------");
+    }
 }
