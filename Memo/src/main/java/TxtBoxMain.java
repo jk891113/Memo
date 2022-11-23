b87d159920d9f34a5931e75087196ec2773d9b1f
SangHwan <skyi0909@naver.com>
2022-11-23 11:42:39 +0900

    첫 실행시 표기만 추가

---
+++ Memo/src/main/java/TxtBoxMain.java
@@ -0,0 +1,10 @@
+
+import presentation.mainInterface;
+
+public class TxtBoxMain {
+    public static void main(String[] args) {
+        mainInterface UI = new mainInterface();
+        UI.run();
+
+    }
+}
---
+++ Memo/src/main/java/presentation/mainInterface.java
@@ -0,0 +1,11 @@
+package presentation;
+
+public class mainInterface {
+    public void run(){
+        System.out.println("-----------------입력 (1번)------------------");
+        System.out.println("-----------------출력 (2번) -----------------");
+        System.out.println("-----------------수정 (3번) -----------------");
+        System.out.println("-----------------삭제 (4번) -----------------");
+        System.out.println("-----------------종료 (5번) -----------------");
+    }
+}
